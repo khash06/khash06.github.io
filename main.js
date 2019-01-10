03f267f874ce5e3ddaa0a3d354aa43e1c0f06849
hashimoto_k <khash06@gmail.com>
2019-01-10 09:54:11 -0800

    refactor for mobile first

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
+let mainNav = document.getElementById('menu');
+
+let navBarToggle = document.getElementById('toggle');
+
+navBarToggle.addEventListener('click', function() {
+    mainNav.classList.toggle('active');
+});
+
 window.onscroll = function(){scrollFunction()};
 
 function scrollFunction() {
-    if(document.body.scrollTop > 640 || document.documentElement.scrollTop > 640) {
-        document.getElementById("navbar").style.padding = "30px 10px";
-        document.getElementById("logo").style.fontSize = "20px";
+    if(document.body.scrollTop > 680 || document.documentElement.scrollTop > 680) {
         document.getElementById("navbar").style.backgroundColor = "#062F4F";
     } else {
-        document.getElementById("navbar").style.padding="40px 10px";
-        document.getElementById("logo").style.fontSize="40px"; 
         document.getElementById("navbar").style.backgroundColor = "#4ABDAC";
-
     }
 }
